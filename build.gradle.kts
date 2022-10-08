2c124459fb1e0b386949355147f997643029555f
Geoff Hackett <ghackett@gmail.com>
2022-10-08 12:02:25 -0400

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
   id("com.android.library") version(libs.versions.android.gradle.get()) apply(false)
   id("org.jetbrains.kotlin.android") version(libs.versions.kotlin.core.get()) apply(false)
 }
+
+allprojects {
+  group = "com.episode6.typed2"
+  version = "2.0.0-SNAPSHOT"
+}
+description = "Type-safe keys for obnoxious key-value stores."
+
+tasks.wrapper {
+  gradleVersion = libs.versions.gradle.core.get()
+  distributionType = Wrapper.DistributionType.ALL
+}
