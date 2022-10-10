f6778fa9790a9273ffe7251a047913f133905a1a
Geoff Hackett <ghackett@gmail.com>
2022-10-10 10:50:41 -0400

    Move async key to its own file

---
+++ core/src/main/kotlin/com/episode6/typed2/AsyncKey.kt
@@ -0,0 +1,19 @@
+package com.episode6.typed2
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+
+interface AsyncKey<T : Any?, GETTER : KeyValueGetter, SETTER : KeyValueSetter> {
+  val name: String
+  suspend fun get(getter: GETTER): T
+  suspend fun set(setter: SETTER, value: T)
+}
+
+fun <T : Any?, GETTER : KeyValueGetter, SETTER : KeyValueSetter> Key<T, GETTER, SETTER>.asAsync(
+  context: CoroutineContext = Dispatchers.Default
+): AsyncKey<T, GETTER, SETTER> = object : AsyncKey<T, GETTER, SETTER> {
+  override val name: String = this@asAsync.name
+  override suspend fun get(getter: GETTER): T = withContext(context) { this@asAsync.get(getter) }
+  override suspend fun set(setter: SETTER, value: T) = withContext(context) { this@asAsync.set(setter, value) }
+}
--- core/src/main/kotlin/com/episode6/typed2/Key.kt
+++ core/src/main/kotlin/com/episode6/typed2/Key.kt
@@ -1,9 +1,5 @@
 package com.episode6.typed2
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.CoroutineContext
-
 interface KeyValueGetter {
   fun contains(name: String): Boolean
 }
@@ -16,26 +12,12 @@ interface Key<T : Any?, GETTER : KeyValueGetter, SETTER : KeyValueSetter> {
   fun set(setter: SETTER, value: T)
 }
 
-interface AsyncKey<T : Any?, GETTER : KeyValueGetter, SETTER : KeyValueSetter> {
-  val name: String
-  suspend fun get(getter: GETTER): T
-  suspend fun set(setter: SETTER, value: T)
-}
-
 interface KeyBuilder {
   val name: String
 }
 
-fun <T : Any?, GETTER : KeyValueGetter, SETTER : KeyValueSetter> Key<T, GETTER, SETTER>.asAsync(
-  context: CoroutineContext = Dispatchers.Default
-): AsyncKey<T, GETTER, SETTER> = object : AsyncKey<T, GETTER, SETTER> {
-  override val name: String = this@asAsync.name
-  override suspend fun get(getter: GETTER): T = withContext(context) { this@asAsync.get(getter) }
-  override suspend fun set(setter: SETTER, value: T) = withContext(context) { this@asAsync.set(setter, value) }
-}
-
 fun <T : Any?, R : Any?, GETTER : KeyValueGetter, SETTER : KeyValueSetter> Key<T, GETTER, SETTER>.mapType(
-  default: ()-> R,
+  default: () -> R,
   mapGet: (T) -> R,
   mapSet: (R) -> T,
 ): Key<R, GETTER, SETTER> = object : Key<R, GETTER, SETTER> {
