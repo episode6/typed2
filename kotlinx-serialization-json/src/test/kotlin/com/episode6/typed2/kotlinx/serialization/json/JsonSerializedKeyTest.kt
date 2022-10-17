75ddb144a04d7f9fb52ad959a3da6518b909c3f6
Geoff Hackett <ghackett@gmail.com>
2022-10-17 18:46:44 -0400

    tests

--- kotlinx-serialization-json/src/test/kotlin/com/episode6/typed2/kotlinx/serialization/json/JsonSerializedKeyTest.kt
+++ kotlinx-serialization-json/src/test/kotlin/com/episode6/typed2/kotlinx/serialization/json/JsonSerializedKeyTest.kt
@@ -7,8 +7,12 @@ import assertk.assertions.*
 import com.episode6.typed2.bundles.BundleKeyNamespace
 import com.episode6.typed2.bundles.RequiredBundleKeyMissing
 import com.episode6.typed2.bundles.get
+import com.episode6.typed2.bundles.set
 import org.junit.Test
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.stub
+import org.mockito.kotlin.verify
 
 @kotlinx.serialization.Serializable data class TestData(val name: String)
 
@@ -32,16 +36,72 @@ class JsonSerializedKeyTest {
     assertThat(result).isNull()
   }
 
+  @Test fun testNullable_get_present() {
+    bundle.setupTestDataKey("testData")
+
+    val result: TestData? = bundle.get(Keys.nullableData)
+
+    assertThat(result).isNotNull().hasName("testData")
+  }
+
+  @Test fun testNullable_set_null() {
+    bundle.set(Keys.nullableData, null)
+
+    verify(bundle).putString("testData", null)
+  }
+
+  @Test fun testNullable_set_obj() {
+    bundle.set(Keys.nullableData, TestData("newData"))
+
+    verify(bundle).putString("testData", jsonTestData("newData"))
+  }
+
   @Test fun testDefault_get_missing() {
     val result: TestData = bundle.get(Keys.defaultData)
 
     assertThat(result).hasName("default")
   }
 
+  @Test fun testDefault_get_present() {
+    bundle.setupTestDataKey("withDefault")
+
+    val result: TestData = bundle.get(Keys.defaultData)
+
+    assertThat(result).hasName("withDefault")
+  }
+
+  @Test fun testDefault_set_obj() {
+    bundle.set(Keys.defaultData, TestData("newData"))
+
+    verify(bundle).putString("withDefault", jsonTestData("newData"))
+  }
+
   @Test fun testRequired_get_missing() {
     assertThat { bundle.get(Keys.requiredData) }
       .isFailure().hasClass(RequiredBundleKeyMissing::class)
   }
+
+  @Test fun testRequired_get_present() {
+    bundle.setupTestDataKey("required")
+
+    val result: TestData = bundle.get(Keys.requiredData)
+
+    assertThat(result).hasName("required")
+  }
+
+  @Test fun testRequired_set_obj() {
+    bundle.set(Keys.requiredData, TestData("newData"))
+
+    verify(bundle).putString("required", jsonTestData("newData"))
+  }
 }
 
-fun Assert<TestData>.hasName(name: String) = prop(TestData::name).isEqualTo(name)
+private fun Assert<TestData>.hasName(name: String) = prop(TestData::name).isEqualTo(name)
+private fun Bundle.setupTestDataKey(name: String) {
+  stub {
+    on { containsKey(name) } doReturn true
+    on { getString(name, null) } doReturn jsonTestData(name)
+  }
+}
+
+private fun jsonTestData(name: String): String =  "{\"name\":\"$name\"}"
