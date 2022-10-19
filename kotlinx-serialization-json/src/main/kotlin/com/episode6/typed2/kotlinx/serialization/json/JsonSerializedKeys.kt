8aa1e6d45f4b9c40fd419151bc7e67b0aacc7b4a
Geoff Hackett <ghackett@gmail.com>
2022-10-19 10:03:57 -0400

    json provider just like gson

--- kotlinx-serialization-json/src/main/kotlin/com/episode6/typed2/kotlinx/serialization/json/JsonSerializedKeys.kt
+++ kotlinx-serialization-json/src/main/kotlin/com/episode6/typed2/kotlinx/serialization/json/JsonSerializedKeys.kt
@@ -6,20 +6,20 @@ import kotlinx.serialization.json.Json
 
 fun <T : Any> PrimitiveKeyBuilder.json(
   default: T,
-  serializer: ()->KSerializer<T>,
-  json: Json = Json,
+  serializer: () -> KSerializer<T>,
+  json: () -> Json = { Json },
 ): PrimitiveKey<T, String?> = json(defaultProvider = { default }, serializer, json)
 
 fun <T : Any> PrimitiveKeyBuilder.json(
   defaultProvider: () -> T,
-  serializer: ()->KSerializer<T>,
-  json: Json = Json,
+  serializer: () -> KSerializer<T>,
+  json: () -> Json = { Json },
 ): PrimitiveKey<T, String?> = json(serializer, json).withDefault(defaultProvider)
 
 fun <T : Any> PrimitiveKeyBuilder.json(
-  serializer: ()->KSerializer<T>,
-  json: Json = Json,
+  serializer: () -> KSerializer<T>,
+  json: () -> Json = { Json },
 ): PrimitiveKey<T?, String?> = string().mapType(
-  mapGet = { it?.let { json.decodeFromString(serializer(), it) } },
-  mapSet = { it?.let { json.encodeToString(serializer(), it) } }
+  mapGet = { it?.let { json().decodeFromString(serializer(), it) } },
+  mapSet = { it?.let { json().encodeToString(serializer(), it) } }
 )
