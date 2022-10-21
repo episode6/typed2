2c8074a464a42036bc557c490bd611914a874e8b
Geoff Hackett <ghackett@gmail.com>
2022-10-20 20:42:13 -0400

    test clear

--- saved-state-handle/src/test/kotlin/com/episode6/typed2/savedstatehandle/SavedStateProviderTest.kt
+++ saved-state-handle/src/test/kotlin/com/episode6/typed2/savedstatehandle/SavedStateProviderTest.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.SavedStateHandle
 import assertk.Assert
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isNull
 import com.episode6.typed2.bundles.BundleKeyNamespace
 import com.episode6.typed2.kotlinx.serialization.bundlizer.bundlized
 import org.junit.Test
@@ -29,8 +30,19 @@ class SavedStateProviderTest {
 
     assertThat(savedStateHandle).hasTestDataObjectWithName("Something")
   }
+
+  @Test fun testClearSavedStateProvider() {
+    savedStateHandle.setSavedStateProvider(Keys.testData) { TestData("Something") }
+    savedStateHandle.clearSavedStateProvider(Keys.testData)
+
+    savedStateHandle.savedStateProvider().saveState()
+
+    assertThat(savedStateHandle).hasNoTestData()
+  }
 }
 
 private fun Assert<SavedStateHandle>.hasTestDataObjectWithName(name: String) = transform {
   it.get<Bundle>("testData")!!.getString("name")
 }.isEqualTo(name)
+
+private fun Assert<SavedStateHandle>.hasNoTestData() = transform { it.get<Bundle>("testData") }.isNull()
