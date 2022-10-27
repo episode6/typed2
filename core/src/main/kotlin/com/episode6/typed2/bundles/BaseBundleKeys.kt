5bd85f214213efbb7fb4bc7f497e2fa1b611b1c7
Geoff Hackett <ghackett@gmail.com>
2022-10-27 12:37:24 -0400

    Add base bundle key namespace (#23)

--- core/src/main/kotlin/com/episode6/typed2/bundles/BaseBundleKeys.kt
+++ core/src/main/kotlin/com/episode6/typed2/bundles/BaseBundleKeys.kt
@@ -1,9 +1,8 @@
 package com.episode6.typed2.bundles
 
-import com.episode6.typed2.AsyncKey
-import com.episode6.typed2.Key
-import com.episode6.typed2.PrimitiveKeyBuilder
-import com.episode6.typed2.nativeKey
+import com.episode6.typed2.*
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
 
 typealias BaseBundleKey<T, BACKED_BY> = Key<T, BACKED_BY, in BaseBundleValueGetter, in BaseBundleValueSetter>
 typealias AsyncBaseBundleKey<T, BACKED_BY> = AsyncKey<T, BACKED_BY, in BaseBundleValueGetter, in BaseBundleValueSetter>
@@ -12,8 +11,23 @@ typealias NativeAsyncBaseBundleKey<T> = AsyncBaseBundleKey<T, T>
 
 interface BaseBundleKeyBuilder : PrimitiveKeyBuilder
 
+/**
+ * This namespace should only be needed if you want to explicitly declare a set of keys
+ * that can be shared between Bundle & PersistableBundle.
+ */
+open class BaseBundleKeyNamespace(private val prefix: String = "") {
+  private class Builder(override val name: String) : BaseBundleKeyBuilder
+
+  protected fun key(name: String): BaseBundleKeyBuilder = Builder(prefix + name)
+
+  protected fun <T : Any, BACKED_BY : Any?> BaseBundleKey<T?, BACKED_BY>.required(): BaseBundleKey<T, BACKED_BY> = asRequired { RequiredBaseBundleKeyMissing(name) }
+  protected fun <T : Any?, BACKED_BY : Any?> BaseBundleKey<T, BACKED_BY>.async(context: CoroutineContext = Dispatchers.Default): AsyncBaseBundleKey<T, BACKED_BY> = asAsync(context)
+}
+
 fun BaseBundleKeyBuilder.double(default: Double): NativeBaseBundleKey<Double> = nativeKey(
   get = { getDouble(name, default) },
   set = { setDouble(name, default) },
   backingDefault = default,
 )
+
+class RequiredBaseBundleKeyMissing(name: String) : IllegalArgumentException("Required key ($name) missing from BaseBundle")
