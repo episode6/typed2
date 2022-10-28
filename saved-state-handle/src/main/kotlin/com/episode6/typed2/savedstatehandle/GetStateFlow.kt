bfd536ce9ff26d1bc6928e6bb7a799e16e22c0e5
Geoff Hackett <ghackett@gmail.com>
2022-10-28 17:27:55 -0400

    Fix the flows in SavedStateHandle to match the shared pref format (#28)

--- saved-state-handle/src/main/kotlin/com/episode6/typed2/savedstatehandle/GetStateFlow.kt
+++ saved-state-handle/src/main/kotlin/com/episode6/typed2/savedstatehandle/GetStateFlow.kt
@@ -4,23 +4,24 @@ import androidx.lifecycle.SavedStateHandle
 import com.episode6.typed2.AsyncKey
 import com.episode6.typed2.Key
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.*
 
-fun <T, BACKED_BY> SavedStateHandle.getStateFlow(
-  scope: CoroutineScope,
+fun <T, BACKED_BY> SavedStateHandle.stateFlow(
   key: Key<T, BACKED_BY, *, *>,
+  scope: CoroutineScope,
+  started: SharingStarted,
 ): StateFlow<T> = getStateFlow(key.name, key.backingTypeInfo.default).run {
-  map { key.mapper.mapGet(it) }.stateIn(scope, SharingStarted.Eagerly, initialValue = key.mapper.mapGet(value))
+  map { key.mapper.mapGet(it) }.stateIn(scope, started, initialValue = key.mapper.mapGet(value))
 }
 
-fun <T, BACKED_BY> SavedStateHandle.getStateFlow(
-  scope: CoroutineScope,
+fun <T, BACKED_BY> SavedStateHandle.sharedFlow(
   key: AsyncKey<T, BACKED_BY, *, *>,
-): StateFlow<T?> = getStateFlow(key.name, key.backingTypeInfo.default)
+  scope: CoroutineScope,
+  started: SharingStarted,
+  replay: Int = 1,
+): SharedFlow<T> = getStateFlow(key.name, key.backingTypeInfo.default)
   .map { key.mapper.mapGet(it) }
-  .stateIn(scope, SharingStarted.Eagerly, null)
+  .conflate()
+  .shareIn(scope, started, replay)
 
 
