6f4466f7908f96cb674929a583963be45c27d2be
Geoff Hackett <ghackett@gmail.com>
2022-10-29 10:09:27 -0400

    actually i don't want casting to be a common thing.

--- core/src/main/kotlin/com/episode6/typed2/bundles/BundleKeys.kt
+++ core/src/main/kotlin/com/episode6/typed2/bundles/BundleKeys.kt
@@ -8,8 +8,6 @@ import kotlin.coroutines.CoroutineContext
 
 typealias BundleKey<T, BACKED_BY> = Key<T, BACKED_BY, in BundleValueGetter, in BundleValueSetter>
 typealias AsyncBundleKey<T, BACKED_BY> = AsyncKey<T, BACKED_BY, in BundleValueGetter, in BundleValueSetter>
-typealias NativeBundleKey<T> = BundleKey<T, T>
-typealias NativeAsyncBundleKey<T> = AsyncBundleKey<T, T>
 typealias BundleProperty<T> = KeyValueDelegate<T, in BundleValueGetter, in BundleValueSetter>
 
 interface BundleKeyBuilder : BaseBundleKeyBuilder
@@ -18,31 +16,40 @@ open class BundleKeyNamespace(private val prefix: String = "") {
 
   protected fun key(name: String): BundleKeyBuilder = Builder(prefix + name)
 
-  protected fun <T : Any, BACKED_BY : Any?> BundleKey<T?, BACKED_BY>.required(): BundleKey<T, BACKED_BY> = asRequired { RequiredBundleKeyMissing(name) }
-  protected fun <T : Any?, BACKED_BY : Any?> BundleKey<T, BACKED_BY>.async(context: CoroutineContext = Dispatchers.Default): AsyncBundleKey<T, BACKED_BY> = asAsync(context)
+  protected fun <T : Any, BACKED_BY : Any?> BundleKey<T?, BACKED_BY>.required(): BundleKey<T, BACKED_BY> =
+    asRequired { RequiredBundleKeyMissing(name) }
+
+  protected fun <T : Any?, BACKED_BY : Any?> BundleKey<T, BACKED_BY>.async(context: CoroutineContext = Dispatchers.Default): AsyncBundleKey<T, BACKED_BY> =
+    asAsync(context)
 }
 
-fun <T: IBinder> BundleKeyBuilder.binder(): BundleKey<T?, IBinder?> = nativeBinder().cast()
-private fun BundleKeyBuilder.nativeBinder(): NativeBundleKey<IBinder?> = nativeKey(
-  get = { getBinder(name) },
-  set = { setBinder(name, it) }
+@Suppress("UNCHECKED_CAST")
+fun <T : IBinder> BundleKeyBuilder.binder(): BundleKey<T?, IBinder?> = nativeBinder().mapType(
+  mapGet = { it as? T },
+  mapSet = { it }
 )
 
 fun BundleKeyBuilder.bundle(default: Bundle): BundleKey<Bundle, Bundle?> = bundle { default }
 fun BundleKeyBuilder.bundle(default: () -> Bundle): BundleKey<Bundle, Bundle?> = bundle().withDefault(default)
-fun BundleKeyBuilder.bundle(): NativeBundleKey<Bundle?> = nativeKey(
+fun BundleKeyBuilder.bundle(): BundleKey<Bundle?, Bundle?> = nativeKey(
   get = { getBundle(name) },
   set = { setBundle(name, it) },
 )
 
-fun BundleKeyBuilder.byte(default: Byte): NativeBundleKey<Byte> = nativeKey(
+fun BundleKeyBuilder.byte(default: Byte): BundleKey<Byte, Byte> = nativeKey(
   get = { getByte(name, default) },
   set = { setByte(name, it) },
   backingDefault = default
 )
+
 fun BundleKeyBuilder.byte(): BundleKey<Byte?, String?> = int().mapType(
   mapGet = { it?.toByte() },
   mapSet = { it?.toInt() }
+)
+
+private fun BundleKeyBuilder.nativeBinder(): BundleKey<IBinder?, IBinder?> = nativeKey(
+  get = { getBinder(name) },
+  set = { setBinder(name, it) }
 )
 
 class RequiredBundleKeyMissing(name: String) : IllegalArgumentException("Required key ($name) missing from bundle")
