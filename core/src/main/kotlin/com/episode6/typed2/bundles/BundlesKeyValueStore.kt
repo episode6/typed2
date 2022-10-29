1767e60a2acfd9072d673a1425417884b4c0fdde
Geoff Hackett <ghackett@gmail.com>
2022-10-28 20:26:23 -0400

    stub new getter/setter methods for now

--- core/src/main/kotlin/com/episode6/typed2/bundles/BundlesKeyValueStore.kt
+++ core/src/main/kotlin/com/episode6/typed2/bundles/BundlesKeyValueStore.kt
@@ -2,17 +2,19 @@ package com.episode6.typed2.bundles
 
 import android.os.Bundle
 import android.os.IBinder
-import com.episode6.typed2.*
+import com.episode6.typed2.get
+import com.episode6.typed2.set
 
 interface BundleValueGetter : BaseBundleValueGetter {
-  fun getBinder(name: String): IBinder?
+  fun getBinder(name: String): IBinder? = TODO()
   fun getBundle(name: String): Bundle?
-  fun getByte(name: String, default: Byte): Byte
+  fun getByte(name: String, default: Byte): Byte = TODO()
 }
+
 interface BundleValueSetter : BaseBundleValueSetter {
-  fun setBinder(name: String, value: IBinder?)
+  fun setBinder(name: String, value: IBinder?) { TODO() }
   fun setBundle(name: String, value: Bundle?)
-  fun setByte(name: String, value: Byte)
+  fun setByte(name: String, value: Byte) { TODO() }
 }
 
 fun <T> BundleValueGetter.get(key: BundleKey<T, *>): T = key.get(this)
