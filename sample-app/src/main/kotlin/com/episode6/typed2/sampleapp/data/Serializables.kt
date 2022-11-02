4928a92e84e81177428d558b68738cab7c57a281
Geoff Hackett <ghackett@gmail.com>
2022-11-02 17:34:09 -0400

    Add some serializable sample classes to sample app (#52)
    Theres never a good time to add these, and I want them in the connected
    tests, so lets just add them now to unblock

---
+++ sample-app/src/main/kotlin/com/episode6/typed2/sampleapp/data/Serializables.kt
@@ -0,0 +1,12 @@
+package com.episode6.typed2.sampleapp.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@kotlinx.serialization.Serializable data class KtsSerializable(val content: String)
+
+data class JavaIoSerializable(val content: String): java.io.Serializable
+
+@Parcelize data class AndroidParcelable(val content: String): Parcelable
+
+data class RegularAssDataClass(val content: String)
