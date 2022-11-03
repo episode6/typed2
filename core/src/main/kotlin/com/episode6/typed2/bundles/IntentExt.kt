001cea559eee41784b79b5aaf17fb63719d4b43d
Geoff Hackett <ghackett@gmail.com>
2022-11-03 16:43:39 -0400

    add tests... then fix impl

--- core/src/main/kotlin/com/episode6/typed2/bundles/IntentExt.kt
+++ core/src/main/kotlin/com/episode6/typed2/bundles/IntentExt.kt
@@ -2,17 +2,24 @@ package com.episode6.typed2.bundles
 
 import android.content.Intent
 import android.os.Bundle
+import com.episode6.typed2.*
+import com.episode6.typed2.KeyValueDelegate
+import kotlin.reflect.KProperty
 
-fun Intent.typedExtras(): TypedBundle = (extras ?: Bundle().also { putExtras(it) }).typed()
 
-// used for get methods so we don't putExtras unnecessarily
-private fun Intent.readOnlyTypedExtras(): TypedBundle = (extras ?: Bundle()).typed()
+fun <T> Intent.getExtra(key: BundleKey<T, *>): T = (extras ?: Bundle()).typed().get(key)
+fun <T> Intent.setExtra(key: BundleKey<T, *>, value: T) = putExtras(Bundle().apply { set(key, value) })
+fun Intent.removeExtra(key: BundleKey<*, *>) = removeExtra(key.name)
+suspend fun <T> Intent.getExtra(key: AsyncBundleKey<T, *>): T = (extras ?: Bundle()).typed().get(key)
+suspend fun <T> Intent.setExtra(key: AsyncBundleKey<T, *>, value: T) = putExtras(Bundle().apply { set(key, value) })
+fun Intent.removeExtra(key: AsyncBundleKey<*, *>) = removeExtra(key.name)
 
-fun <T> Intent.getExtra(key: BundleKey<T, *>): T = readOnlyTypedExtras().get(key)
-fun <T> Intent.setExtra(key: BundleKey<T, *>, value: T) = typedExtras().set(key, value)
-fun Intent.removeExtra(key: BundleKey<*, *>) = typedExtras().remove(key)
-suspend fun <T> Intent.getExtra(key: AsyncBundleKey<T, *>): T = readOnlyTypedExtras().get(key)
-suspend fun <T> Intent.setExtra(key: AsyncBundleKey<T, *>, value: T) = typedExtras().set(key, value)
-fun Intent.removeExtra(key: AsyncBundleKey<*, *>) = typedExtras().remove(key)
+fun <T> Intent.extraProperty(key: BundleKey<T, *>): IntentKeyValueDelegate<T> = IntentKeyValueDelegate(key, this)
 
-fun <T> Intent.extraProperty(key: BundleKey<T, *>): BundleProperty<T> = typedExtras().property(key)
+class IntentKeyValueDelegate<T : Any?>(
+  private val key: BundleKey<T, *>,
+  private val intent: Intent,
+) {
+  operator fun getValue(thisRef: Any?, property: KProperty<*>): T = intent.getExtra(key)
+  operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) = intent.setExtra(key, value)
+}
---
+++ sample-app/src/androidTest/kotlin/com/episode6/typed2/sampleapp/IntentInstrumentedTest.kt
@@ -0,0 +1,52 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package com.episode6.typed2.sampleapp
+
+import android.content.Intent
+import assertk.assertAll
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNull
+import com.episode6.typed2.async
+import com.episode6.typed2.bundles.BundleKeyNamespace
+import com.episode6.typed2.bundles.getExtra
+import com.episode6.typed2.bundles.setExtra
+import com.episode6.typed2.int
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class IntentInstrumentedTest {
+  private object Keys : BundleKeyNamespace() {
+    val int = key("int").int()
+    val intAsync = key("intAsync").int().async()
+  }
+
+  private val intent = Intent()
+
+  @Test fun testInt() {
+    assertThat(intent.getExtra(Keys.int)).isNull()
+
+    intent.setExtra(Keys.int, 101)
+    val raw = intent.getStringExtra("int")
+    val typed = intent.getExtra(Keys.int)
+
+    assertAll {
+      assertThat(raw).isEqualTo("101")
+      assertThat(typed).isEqualTo(101)
+    }
+  }
+
+  @Test fun testAsyncInt() = runTest {
+    assertThat(intent.getExtra(Keys.intAsync)).isNull()
+
+    intent.setExtra(Keys.intAsync, 101)
+    val raw = intent.getStringExtra("intAsync")
+    val typed = intent.getExtra(Keys.intAsync)
+
+    assertAll {
+      assertThat(raw).isEqualTo("101")
+      assertThat(typed).isEqualTo(101)
+    }
+  }
+}
